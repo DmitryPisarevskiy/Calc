4ab8fff96ca7d4aca3d5c143e35dd21d618151ea
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2021-10-19 22:20:29 +0500

    tests

--- app/src/main/java/com/dmitry/pisarevskiy/calc/MainActivity.java
+++ app/src/main/java/com/dmitry/pisarevskiy/calc/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!tvInput.getText().equals("")) {
                     try {
-                        tvOutput.setText(String.valueOf(result(tvInput.getText().toString())));
+                        tvOutput.setText(String.valueOf(Logic.result(tvInput.getText().toString())));
                     } catch (IllegalArgumentException | StringIndexOutOfBoundsException | NullPointerException e) {
                         e.printStackTrace();
                         System.out.println("Неверный аргумент");
@@ -88,79 +88,5 @@ public class MainActivity extends AppCompatActivity {
                 tvInput.setText(str);
             }
         });
-    }
-
-    private float result(String text) throws IllegalArgumentException {
-        if (text.charAt(0)=='(' && text.charAt(text.length()-1)==')' && checkedBrackets(text.substring(1,text.length()-1))) {
-            return (result(text.substring(1,text.length()-1)));
-        }
-        if (text.charAt(0)=='+' || text.charAt(0)=='-') {
-            return (result("0"+text));
-        }
-        if (text.charAt(text.length()-1)=='+' || text.charAt(text.length()-1)=='-') {
-            return (result(text+"0"));
-        }
-        if (isFloat(text)) {
-            return Float.parseFloat(text);
-        }
-        if (splitBySign(text,'+').length==2) {
-            return (result(splitBySign(text,'+')[0]) + result(splitBySign(text,'+')[1]));
-        }
-        if (splitBySign(text,'-').length==2) {
-            return (result(splitBySign(text,'-')[0]) - result(splitBySign(text,'-')[1]));
-        }
-        if (splitBySign(text,'*').length==2) {
-            return (result(splitBySign(text,'*')[0]) * result(splitBySign(text,'*')[1]));
-        }
-        if (splitBySign(text,'/').length==2) {
-            return (result(splitBySign(text,'/')[0]) / result(splitBySign(text,'/')[1]));
-        }
-        throw new IllegalArgumentException();
-    }
-
-    public static boolean isFloat(String value) {
-        return value.matches("\\d+(\\.\\d+)?");
-    }
-
-    public static String[] splitBySign(String text, char sign) {
-        int i=0;
-        int bracketsCounter=0;
-        while (i<text.length()) {
-            if (text.charAt(i)=='(') {
-                bracketsCounter=1;
-                while (bracketsCounter!=0) {
-                    i++;
-                    if (text.charAt(i)=='(') {
-                        bracketsCounter+=1;
-                    }
-                    if (text.charAt(i)==')') {
-                        bracketsCounter-=1;
-                    }
-                }
-            }
-            if (text.charAt(i)==sign) {
-                return new String[]{text.substring(0,i),text.substring(i+1,text.length())};
-            }
-            i++;
-        }
-        return new String[]{text};
-    }
-
-    public static boolean checkedBrackets(String text) {
-        int bracketsCounter=0;
-        int i=0;
-        while (i<text.length()) {
-            if (text.charAt(i)=='(') {
-                bracketsCounter+=1;
-            }
-            if (text.charAt(i)==')') {
-                bracketsCounter-=1;
-            }
-            if (bracketsCounter<0) {
-                return false;
-            }
-            i++;
-        }
-        return true;
     }
 }
--- app/src/test/java/com/dmitry/pisarevskiy/calc/ExampleUnitTest.kt
+++ app/src/test/java/com/dmitry/pisarevskiy/calc/ExampleUnitTest.kt
@@ -1,16 +1,29 @@
 package com.dmitry.pisarevskiy.calc
 
+import com.dmitry.pisarevskiy.calc.Logic.*
 import org.junit.Assert
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        Assert.assertEquals(4, (2 + 2).toLong())
+        Assert.assertEquals(4, result("2+2").toInt())
+    }
+
+    @Test
+    fun multiplying_isNotCorrect() {
+        Assert.assertNotEquals(6, result("2*2").toInt())
+    }
+
+    @Test
+    fun arrayResult_isCorrect() {
+        val answers = arrayOf(2,6,21)
+        val calculations = arrayOf(result("8-2*3").toInt(),result("6*7-6*6").toInt(), result("7*(4-1)").toInt())
+        Assert.assertArrayEquals(answers,calculations)
+    }
+
+    @Test
+    fun dividing_isCorredt() {
+        Assert.assertSame(3, result("9/3").toInt())
     }
 }
